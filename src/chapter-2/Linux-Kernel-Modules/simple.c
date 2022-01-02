8aa5353fa6792993f76f5d2f7f8da0aa9b92bcf3
lusm554 <loveyousomuch554@gmail.com>
2022-01-02 18:47:30 +0000

    fetch some info from kernel

--- src/chapter-2/Linux-Kernel-Modules/simple.c
+++ src/chapter-2/Linux-Kernel-Modules/simple.c
@@ -1,20 +1,44 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/hash.h> // GOLDEN_RATIO_PRIME
+#include <linux/gcd.h> // gcd
+#include <asm/param.h> // HZ
+#include <linux/jiffies.h> // jiffies
 
-// This function is called when the module is loaded.
-static int simple_init(void) {
+unsigned long int JIFFIES_AT_INIT;
+
+/* This function is called when the module is loaded. */
+int simple_init(void) {
   printk(KERN_INFO "Loading Kernel Module\n");
+
+  unsigned long int a = 3300;
+  unsigned int b = 24;
+  JIFFIES_AT_INIT = jiffies;
+
+  /* Print GOLDEN_RATIO_PRIME and GCD of a and b. */
+  printk(KERN_INFO "GOLDEN_RATIO_PRIME: %u\n", GOLDEN_RATIO_PRIME);
+  printk(KERN_INFO "Greatest common divisor of %u and %u: %llu\n", a, b, gcd(a, b));
+
+  /* Print jiffies and HZ. */
+  printk(KERN_INFO "Jiffies in init: %u\n", jiffies);
+  printk(KERN_INFO "HZ: %u\n", HZ);
 
   return 0;
 }
 
-// This function is called when the module is removed.
-static void simple_exit(void) {
+/* This function is called when the module is removed. */
+void simple_exit(void) {
+  int time = (JIFFIES_AT_INIT - jiffies) * 1000 / HZ; // TODO: FIX
+
+  /* Print jiffies and number of seconds that have elapsed since module loaded. */
+  printk(KERN_INFO "Jiffies in exit: %u\n", time);
+  printk(KERN_INFO "Number of seconds since module loaded: %u\n", jiffies);
+
   printk(KERN_INFO "Removing Kernel Module\n");
 }
 
-// Macros for registering module entry and exit points.
+/* Macros for registering module entry and exit points. */
 module_init(simple_init);
 module_exit(simple_exit);
 
