20fd09f2506dd19bcbcb83057a1d97d089d071a4
lusm554 <loveyousomuch554@gmail.com>
2022-01-04 14:53:52 +0000

    raw version of hello module

--- src/chapter-2/Linux-Kernel-Modules/hello.c
+++ src/chapter-2/Linux-Kernel-Modules/hello.c
@@ -1,7 +1,7 @@
 #include <linux/init.h>
-#include <linuxkernel.h/>
+#include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/prof_fs.h>
+#include <linux/proc_fs.h>
 #include <linux/uaccess.h>
 
 #define BUFFER_SIZE 128
@@ -20,6 +20,8 @@ int proc_init(void) {
   /* create the "/proc/hello" entry */
   proc_create(PROC_NAME, 0666, NULL, &proc_ops);
   
+  printk(KERN_INFO "Loading Kernel Module \"hello\"\n");
+
   return 0;
 }
 
@@ -27,10 +29,35 @@ int proc_init(void) {
 void proc_exit(void) {
   /* removes the "/proc/hello" entry. */
   remove_proc_entry(PROC_NAME, NULL);
+
+  printk(KERN_INFO "Removing Kernel Module \"hello\"\n"); 
 }
 
 /* This function is called each time "/proc/hello" is read. */
 ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos) {
+  int rv = 0;
+  char buffer[BUFFER_SIZE];
+  static int completed = 0;
 
+  if (completed) {
+    completed = 0;
+    return 0;
+  }
+
+  completed = 1;
+
+  rv = sprintf(buffer, "Hello World\n");
+
+  /* copies kernel space buffer to user space usr_buf */
+  copy_to_user(usr_buf, buffer, rv);
+
+  return rv;
 }
+
+module_init(proc_init);
+module_exit(proc_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Hello Module");
+MODULE_AUTHOR("SGG");
 
