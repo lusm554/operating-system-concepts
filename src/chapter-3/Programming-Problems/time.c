d8b82fd8ddf0d3f6a41c2638e3dc2accf61e2eb5
lusm554 <loveyousomuch554@gmail.com>
2022-01-14 11:35:31 +0000

    fixed problem with elasped time

--- src/chapter-3/Programming-Problems/time.c
+++ src/chapter-3/Programming-Problems/time.c
@@ -7,15 +7,14 @@ int main(void) {
   struct timeval start, end;
 
   gettimeofday(&start, NULL);
-  printf("Start seconds: %ld.%ld\n", start.tv_sec, start.tv_usec);
-
-  sleep(30);
+  printf("Waiting %u seconds\n", 5);
+    
+  sleep(5);
 
   gettimeofday(&end, NULL);
 
-  long unsigned diff = (end.tv_sec - start.tv_sec) + 1e-6*(end.tv_usec - start.tv_usec);
-  printf("End seconds: %ld.%ld,\ndiff: %ld\n", end.tv_sec, end.tv_usec, diff);
-  
+  int elapsed = ((end.tv_sec - start.tv_sec) * 1000000) + (end.tv_usec - start.tv_usec);
+  printf("elapsed: %u\n", elapsed);
 
   return 0;
 }
