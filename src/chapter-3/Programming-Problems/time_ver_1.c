e1e2c7e579bb3155a7b2536fe6e264d98d5029be
lusm554 <loveyousomuch554@gmail.com>
2022-01-16 22:09:44 +0000

    shm work!

--- src/chapter-3/Programming-Problems/time_ver_1.c
+++ src/chapter-3/Programming-Problems/time_ver_1.c
@@ -1,26 +1,78 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+#define SIZE sizeof(long long int) 
+#define NAME "/time_counter"
 
 int elapsed(struct timeval *start, struct timeval *end);
 char *to_str_view(int all_msecs);
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    printf("Error, command not specific!\n");
+    fprintf(stderr, "usage: ./time [command_to_execute]\n");
     return 1;
   }
+  
+  /* ELAPSED TIME */
+  struct timeval start, end;                                                                        
 
-  struct timeval start, end;
+  gettimeofday(&start, NULL);                                                                       
 
-  gettimeofday(&start, NULL);
-  sleep(3);
-  gettimeofday(&end, NULL);
+  sleep(3);                                                                                         
+  gettimeofday(&end, NULL);                                                                       
 
-  char *view = to_str_view(elapsed(&start, &end));
+  int el = elapsed(&start, &end);  
 
+  char *view = to_str_view(el);
   printf("%s", view);
+  
+  /* SHARED MEMORY */
+  int fd = shm_open(NAME, O_CREAT | O_EXCL | O_RDWR, 0666);
+  if (fd < 0) {
+    perror("shm_open()");
+    return EXIT_FAILURE;
+  }
+
+  if (ftruncate(fd, SIZE) < 0) {
+    perror("ftruncate()");
+    return EXIT_FAILURE; 
+  }
+
+  int *ptr = mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+  printf("sender mapped address: %p\n", ptr);
+
+  // write to the shm object
+  ptr[0] = el;
+  
+  munmap(ptr, SIZE);
+  close(fd);
+
+  /* TEST RECEIVING DATE FROM SHM */
+  {
+    int fd = shm_open(NAME, O_RDONLY, 0666);
+    if (fd < 0) {
+      perror("shm_open() recv");
+      return EXIT_FAILURE;
+    }
+    
+    int *ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, fd, 0);
+    printf("receiver mapped address: %p\n", ptr);
+
+    printf("data: %d\n", ptr[0]);
+
+    munmap(ptr, SIZE);
+    close(fd);
+    shm_unlink(NAME);
+  }
+
   return 0;
 }
 
