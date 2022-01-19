bbc848d0272150d2d6f992560f247e8e26a19062
lusm554 <loveyousomuch554@gmail.com>
2022-01-19 19:09:56 +0000

    bitmap

--- src/chapter-3/Programming-Problems/pid_manager.c
+++ src/chapter-3/Programming-Problems/pid_manager.c
@@ -4,19 +4,43 @@
 #define MAX_PID 5000
 #define SIZE MAX_PID - MIN_PID
 
-int main(void) {
-  int bitmap[SIZE];
+int alloc_map(void);
+int alloc_pid(void);
+void release_pid(int pid);
 
+int bitmap[SIZE];
+
+int main(void) {
   printf("%zu\n", sizeof bitmap);
 
-  for (int i = 0; i < (MAX_PID - MIN_PID); i++) {
-    bitmap[i] = 1;
+  alloc_map();
+  alloc_pid();
+
+  for (int i = 0; i < 10; i++) {
+    printf("bitmap[%d] = %d\n", i, bitmap[i]);
   }
 
   return 0;
 }
 
-int alloc_map(void) {}
-int allo_pid(void) {}
-void release_pid(int pid) {}
+int alloc_map(void) {
+  for (int i = 0; i < MAX_PID - MIN_PID; i++) {
+    bitmap[i] = 0;
+  }
+  bitmap[0] = 1;
+  return 1;
+}
+
+int alloc_pid(void) {
+  int i = 1;
+  while (bitmap[i] != 0) {
+    bitmap[i] = 1;
+    printf("%d\n", i);
+    i++;
+  }
+  return 1;
+}
+
+void release_pid(int pid) {
+}
 
