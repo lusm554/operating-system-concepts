ed66f7685692a5c121ef482c624e50ec0eb9d109
lusm554 <loveyousomuch554@gmail.com>
2022-01-20 20:38:57 +0000

    need to use bitwise operators for this

--- src/chapter-3/Programming-Problems/pid_manager.c
+++ src/chapter-3/Programming-Problems/pid_manager.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MIN_PID 300
 #define MAX_PID 5000
@@ -8,39 +9,51 @@ int alloc_map(void);
 int alloc_pid(void);
 void release_pid(int pid);
 
-int bitmap[SIZE];
+int bitmap[SIZE] = {0};
 
 int main(void) {
+  alloc_map();
+
   printf("%zu\n", sizeof bitmap);
 
-  alloc_map();
-  alloc_pid();
+  for (int i = 0; i < 5; i++) {
+    int t = alloc_pid();
+  }
 
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 6; i++) {
+    printf("bitmap[%d] = %d\n", i, bitmap[i]);
+  }
+
+  for (int i = 0; i < 5; i++) {
+    release_pid(MIN_PID + i);
+  }
+
+  printf("\n");
+  for (int i = 0; i < 6; i++) {
     printf("bitmap[%d] = %d\n", i, bitmap[i]);
   }
 
   return 0;
 }
 
+// TODO: fix allocation of map 
 int alloc_map(void) {
-  for (int i = 0; i < MAX_PID - MIN_PID; i++) {
-    bitmap[i] = 0;
-  }
-  bitmap[0] = 1;
   return 1;
 }
 
 int alloc_pid(void) {
-  int i = 1;
-  while (bitmap[i] != 0) {
-    bitmap[i] = 1;
-    printf("%d\n", i);
-    i++;
+  for (int i = 0; i < SIZE; i++) {
+    if (bitmap[i] == 0) {
+      bitmap[i] = MIN_PID + i;
+      return MIN_PID + i; 
+    }
   }
-  return 1;
+
+  return -1;
 }
 
 void release_pid(int pid) {
+  if (!(pid >= MIN_PID & pid <= MAX_PID)) return;
+  bitmap[pid - MIN_PID] = 0;
 }
 
